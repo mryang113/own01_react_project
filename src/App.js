cd9de468907114d241bd1ce1a2756d7cc8a95e12
mryang113 <mryang113@163.com>
2020-03-13 21:02:11 +0800

    更新

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
+import {Button} from 'antd'
 
 export default class App extends Component {
   render() {
     return (
       <div>
         app....aaa
+        <br/>
+        <br/>
+        <Button>点我</Button><br/>
+        <Button type="primary">Primary</Button><br/>
+        <Button>Default</Button><br/>
+        <Button type="dashed">Dashed</Button><br/>
+        <Button type="link">Link</Button><br/>
       </div>
     )
   }
