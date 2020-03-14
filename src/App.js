4fac28c59d2b88fa80eb7673a5a823a6a643934f
mryang113 <mryang113@163.com>
2020-03-14 23:03:33 +0800

    redux_没有Action 版

--- src/App.js
+++ src/App.js
@@ -1,62 +1,14 @@
 import React, { Component } from 'react'
+import Count from './components/count'
 
 export default class App extends Component {
-
-  state = {count: 0}
-  
-
-  //加法
-  increment = () => {
-    let {value} = this.refs.numberNode
-    let {count} = this.state
-    count += value*1
-    this.setState({count})
-  }
-
-  //减法
-  decrement = () => { 
-    let {value} = this.refs.numberNode
-    let {count} = this.state
-    count -= value*1
-    this.setState({count})
-  }
-
-  //当前为奇数时再加
-  incrementIfOdd = () => {
-    let {value} = this.refs.numberNode
-    let {count} = this.state
-    if(count % 2 === 1){
-      count += value*1
-      this.setState({count})
-    } 
-  }
-
-  //延迟0.5秒加
-  incrementAsync = () => {
-    let {value} = this.refs.numberNode
-    let {count} = this.state
-    setTimeout(() => {
-      count += value*1
-      this.setState({count})
-    }, 1000);
-  }
-
   render() {
-    const {count} = this.state
+    const {store} = this.props
     return (
       <div>
-        {/* <h2>当前总数为: <span className='color'>{count}</span> </h2> */}
-        <h2>当前总数为: <span style={{color:'#f66'}}>{count}</span> </h2>
-        <select ref="numberNode">
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-        </select>&nbsp;
-        <button onClick={this.increment}>+</button>&nbsp;
-        <button onClick={this.decrement}>—</button>&nbsp;
-        <button onClick={this.incrementIfOdd}>increment if odd</button>&nbsp;
-        <button onClick={this.incrementAsync}>increment async</button>
+        <Count store={store}/>
       </div>
     )
   }
 }
+
---
+++ src/components/count.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+
+export default class Count extends Component {
+
+  //加法
+  increment = () => {
+    //1.获取用户输入
+    let {value} = this.refs.numberNode
+    //2.通知redux加1
+    // 这里正常component组件给 ===>> redux传参用 dispatch这个方法
+    this.props.store.dispatch({type:'increment',data:value*1}) 
+  }
+
+  //减法
+  decrement = () => { 
+    let {value} = this.refs.numberNode
+    this.props.store.dispatch({type:'decrement',data:value*1})
+  }
+
+  //当前为奇数时再加
+  incrementIfOdd = () => {
+    let {value} = this.refs.numberNode
+    // 这里想拿到状态值 用store.getState()
+    let count = this.props.store.getState()
+    if(count % 2 === 1){
+      this.props.store.dispatch({type:'increment',data:value*1}) 
+    } 
+  }
+
+  //延迟0.5秒加
+  incrementAsync = () => {
+    let {value} = this.refs.numberNode
+    setTimeout(() => {
+      this.props.store.dispatch({type:'increment',data:value*1}) 
+    }, 1000);
+  }
+
+  render() {
+    const {store} = this.props
+    return (
+      <div>
+        {/* <h2>当前总数为: <span className='color'>{count}</span> </h2> */}
+        <h2>当前总数为: <span style={{color:'#f66'}}>{store.getState()}</span> </h2>
+        <select ref="numberNode">
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+        </select>&nbsp;
+        <button onClick={this.increment}>+</button>&nbsp;
+        <button onClick={this.decrement}>—</button>&nbsp;
+        <button onClick={this.incrementIfOdd}>increment if odd</button>&nbsp;
+        <button onClick={this.incrementAsync}>increment async</button>
+      </div>
+    )
+  }
+}
