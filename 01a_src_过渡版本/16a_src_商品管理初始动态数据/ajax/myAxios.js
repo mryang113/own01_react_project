bb5e976e9102af79d19ba99b8564a8da04c64b3c
mryang113 <mryang113@163.com>
2020-03-22 20:59:10 +0800

    商品管理动态数据展示

---
+++ 01a_src_过渡版本/16a_src_商品管理初始动态数据/ajax/myAxios.js
@@ -0,0 +1,49 @@
+import axios from 'axios'
+import qs from 'querystring'
+import {message} from 'antd';
+import {createDeleteUserAction} from '../redux/actions/login'
+import {createSaveTitleAction} from '../redux/actions/header'
+import store from '../redux/store'
+
+// 配置请求的基准URL地址
+axios.defaults.baseURL = 'http://localhost:3000'
+
+
+//请求拦截器
+axios.interceptors.request.use((config)=>{
+  let {method,data} = config
+  
+  // 1-token- 获取token
+  let {token} = store.getState().userInfo
+  if(token){
+    config.headers.Authorization = 'atguigu_' + token  // 服务器认这个请求头携带的参数,百度,糯米用的就是这个
+  }
+  // 2-token 携带token 
+  if(method.toLowerCase() === 'post' && data instanceof Object){
+    // console.log('666',qs.stringify(data));
+    config.data =  qs.stringify(data)
+  }
+  return config
+});
+
+// 响应拦截器
+axios.interceptors.response.use(
+  (response) => {
+    
+    return response.data
+  },
+  (err) => {
+    //1,服务器没响应 2, 服务器返回的状态码不是2开头
+    console.log(err);// 验证token失效时 输出中err包含着响应response里有status码是 401,好做判断;
+    if(err.response.status === 401){
+      message.error()
+      store.dispatch(createDeleteUserAction())
+      store.dispatch(createSaveTitleAction(''));//如果身份token过期,就先提示从新登陆,然后直接store分发一个删除的action
+    }
+    //这里用 message 要在上面引入
+    message.warning(err.message)
+    return new Promise(()=>{})
+  }
+);
+
+export default axios  // 这是加工过的axios自带拦截器功能;
---
+++ 01a_src_过渡版本/16a_src_商品管理初始动态数据/containers/header/header.jsx
@@ -0,0 +1,106 @@
+import React, { Component } from 'react'
+import {Button,Modal} from 'antd'
+import screenfull from 'screenfull'
+import { connect } from 'react-redux'
+import dayjs from 'dayjs'
+import {
+  FullscreenOutlined,
+  FullscreenExitOutlined,
+  ExclamationCircleOutlined
+} from '@ant-design/icons'
+import {createDeleteUserAction} from '../../redux/actions/login'
+import {createSaveTitleAction} from '../../redux/actions/header'
+import {reqWeather} from '../../ajax'
+import './css/header.less'
+const { confirm } = Modal;
+
+class Header extends Component {
+
+  state = {
+    isFull: false, //标识是否全屏 
+    date:dayjs().format('YYYY年 MM月 DD日 HH:mm:ss'),
+    dayPictureUrl:'',
+    temperature:'',
+    weather:''
+  }
+
+  fullScreen = () => {
+    //让网页全屏
+    screenfull.toggle();
+  }
+
+  logOut = ()=>{
+    // this.props.logout() // 测试之前写的,可以用
+		confirm({
+			title: '确定退出吗？',
+			icon: <ExclamationCircleOutlined />,
+			content: '退出后要重新登录',
+			okText:'确定',
+			cancelText:'取消',
+			onOk:()=> { //确认按钮的回调
+        this.props.logout()
+        this.props.deleteTitle('') //这个是让他点击退出的时候,redux里的state里的title 清空
+			},
+		});
+  }
+  
+  getWeather = async() => {
+    let result = await reqWeather()
+		const {dayPictureUrl,weather,temperature} = result
+		this.setState({dayPictureUrl,weather,temperature})
+  }
+
+  componentDidMount(){
+    //标识是否全屏 
+    screenfull.onchange(() => {
+      let isFull = !this.state.isFull
+      this.setState({isFull})
+    })
+    //开启更新时间的定时器
+		this.timer = setInterval(() => {
+			this.setState({date:dayjs().format('YYYY年 MM月 DD日 HH:mm:ss')})
+    },1000);
+    //请求天气信息 
+    this.getWeather()
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.timer)
+  }
+  
+  render() {
+    const {isFull,date,dayPictureUrl,weather,temperature} = this.state
+    return (
+      <div className="header">
+        <div className="header-top">
+          <Button onClick={this.fullScreen} size="small">
+            {isFull ? <FullscreenExitOutlined /> : <FullscreenOutlined />}
+          </Button>
+          <span className="user">欢迎, {this.props.username}</span>
+          <Button onClick={this.logOut} type="link">退出登录</Button>
+        </div>
+        <div className="header-bottom">
+          <div className="bottom-left">
+            <h2>{this.props.title}</h2> 
+          </div>
+          <div className="bottom-right">
+          <span>{date}</span>
+            <img src={dayPictureUrl} alt="pic"/>
+            <span>{weather}温度: {temperature}</span>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default connect(
+(state) => ({
+  username: state.userInfo.user.username,
+  title:state.title
+}),//传递状态
+{
+  logout: createDeleteUserAction,
+  deleteTitle:createSaveTitleAction
+}//传递操作状态的方法
+)(Header)
